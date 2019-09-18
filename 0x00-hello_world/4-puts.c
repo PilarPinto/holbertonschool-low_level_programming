cd638d724ef5d5a751ebde9bebbc97fc5c5818af
PilarPinto <piapintoch@unal.edu.co>
2019-09-18 18:48:15 +0000

    adding some comments

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
- 
-int main (void){
-   puts("\"Programming is like building a multilingual puzzle");
-   return (0);
+/*
+ *Somme comments in the head
+ *
+ */
+int main (void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
