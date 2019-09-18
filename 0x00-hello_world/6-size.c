245ef8410f02d69f43019c3d1ce771a5cc1711c9
PilarPinto <piapintoch@unal.edu.co>
2019-09-18 18:27:30 +0000

    Adding the void

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
-int main()
+int main(void)
 {
-  char charType;
-  int intType;
-  long int lintType;
-  long long int llintType;
-  float floatType;
+  char charT;
+  int intT;
+  long int lintT;
+  long long int llintT;
+  float floatT;
   
-  printf("Size of a char: %ld byte(s)\n",sizeof(charType));
-  printf("Size of an int: %d byte(s)\n",sizeof(intType));
-  printf("Size of a long int: %ld byte(s)\n",sizeof(lintType));
-  printf("Size of a long long int: %ld byte(s)\n",sizeof(llintType));
-  printf("Size of a float: %ld byte(s)\n",sizeof(floatType));
+  printf("Size of a char: %ld byte(s)\n",sizeof(charT));
+  printf("Size of an int: %ld byte(s)\n",sizeof(intT));
+  printf("Size of a long int: %ld byte(s)\n",sizeof(lintT));
+  printf("Size of a long long int: %ld byte(s)\n",sizeof(llintT));
+  printf("Size of a float: %ld byte(s)\n",sizeof(floatT));
   return 0;
 }
