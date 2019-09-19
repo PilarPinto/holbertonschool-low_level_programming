9c8f533cf35fb96e93f6b2de0147b3d26751d12b
PilarPinto <piapintoch@unal.edu.co>
2019-09-19 19:42:22 +0000

    Ading some comment

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch;
@@ -9,6 +14,5 @@ if (ch != 'q' && ch != 'e')
 putchar(ch);
 }
 putchar('\n');
-
 return (0);
 }
