879da5cc377bf7d9ccdcd989217029e733cc13ee
PilarPinto <piapintoch@unal.edu.co>
2019-09-19 19:36:52 +0000

    Adding some comments

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 char ch;
@@ -12,6 +17,5 @@ for (ch = 'A' ; ch <= 'Z' ; ch++)
 putchar(ch);
 }
 putchar('\n');
-
 return (0);
 }
