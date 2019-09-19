13a80762fb91877cf89246d60d767453a9846d49
PilarPinto <piapintoch@unal.edu.co>
2019-09-19 18:31:48 +0000

    Split the printf

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -9,23 +9,30 @@
  */
 int main(void)
 {
-int n;
 
+int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 int lsd = n % 10;
+printf("Last digit of");
+printf("%d is", n)
+printf("%d", lsd)
+
 if (lsd > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, lsd);
+printf("and is greater than 5\n");
 }
+
 else if (lsd == 0)
 {
-printf("Last digit of %d is %d and is 0\n", n, lsd);
+printf("and is 0\n");
 }
+
 else if (lsd < 6 && n != 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n, lsd);
+printf("and is less than 6 and not 0\n");
 }
+
 return (0);
 }
