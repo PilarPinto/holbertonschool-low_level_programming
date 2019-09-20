30a0eff4b7f41582127228306072b760dc29cec5
PilarPinto <piapintoch@unal.edu.co>
2019-09-20 19:24:56 +0000

    Betty is beuty

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,8 +3,8 @@
 #include <time.h>
 
 /**
- * main - Entry point
- *
+ * main - Using module of 10 to obtain the 
+ * last number and conditionals
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -22,17 +22,17 @@ printf(" %d", lsd);
 
 if (lsd > 5)
 {
-printf(" and is greater than 5\n");
+	printf(" and is greater than 5\n");
 }
 
 else if (lsd == 0)
 {
-printf(" and is 0\n");
+	printf(" and is 0\n");
 }
 
 else if (lsd < 6)
 {
-printf(" and is less than 6 and not 0\n");
+	printf(" and is less than 6 and not 0\n");
 }
 
 return (0);
