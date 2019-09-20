7912f930b2ae3b3709be034392460d0d0863c889
PilarPinto <piapintoch@unal.edu.co>
2019-09-20 19:39:03 +0000

    Betty is beauty

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Using decrement for print
+ * an alphabet mirror
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 char ch;
 for (ch = 'z' ; ch >= 'a' ; ch--)
-putchar(ch);
+	putchar(ch);
 putchar('\n');
 return (0);
 }
