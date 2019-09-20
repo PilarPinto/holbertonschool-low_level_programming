9b82555810e0f38c11a5df3d3722b79e0021a9d9
PilarPinto <piapintoch@unal.edu.co>
2019-09-20 19:38:26 +0000

    Betty is beauty

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Print recursively and 
+ * conditonal of q and e
  * Return: Always 0 (Success)
  */
 int main(void)
@@ -10,8 +10,8 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-if (ch != 'q' && ch != 'e')
-putchar(ch);
+	if (ch != 'q' && ch != 'e')
+		putchar(ch);
 }
 putchar('\n');
 return (0);
