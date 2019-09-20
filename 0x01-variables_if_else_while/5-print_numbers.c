2b4765a65b2a1a6376721a5cd3d1b1be56a99651
PilarPinto <piapintoch@unal.edu.co>
2019-09-20 19:38:38 +0000

    Betty is beauty

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Print character of 
+ * numbers
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 char ch;
 for (ch = '0' ; ch <= '9' ; ch++)
-putchar(ch);
+	putchar(ch);
 putchar('\n');
 
 return (0);
