746884843693cdc5008c15327b07ced61cf8d224
PilarPinto <piapintoch@unal.edu.co>
2019-09-20 19:38:57 +0000

    Betty is beauty

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
+ * main - Print int type variables 
+ * with putchar function
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int number;
 for (number = '0' ; number <= '9' ; number++)
-putchar(number);
+	putchar(number);
 putchar('\n');
 
 return (0);
