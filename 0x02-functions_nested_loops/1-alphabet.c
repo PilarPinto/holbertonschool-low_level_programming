da30f3115a82eb7f6288a6a6221deff99e0111b3
PilarPinto <piapintoch@unal.edu.co>
2019-09-23 15:57:18 +0000

    Fixin the void function in white  spaces

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,18 +3,19 @@
  *main-Using a void function to print alphabet
  *Return: 0
  */
+int main(void)
+{
+print_alphabet();
+getchar ();
+return (0);
+}
 
-int main(void) 
-{ 
-	print_alphabet(); 
-	getchar(); 
-	return 0; 
-} 
+
 void print_alphabet(void)
-{   
-	char ch;
-	for (ch = 'a'; ch < 'z'; ch++)
-		putchar(ch);
-  
-	putchar('\n');  
+{
+char ch;
+
+for (ch = 'a'; ch < 'z'; ch++)
+putchar (ch);
+putchar ('\n');
 }
