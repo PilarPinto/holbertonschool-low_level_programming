c7ebf3d73bb0312241eef3795bf77a6a76d6169f
PilarPinto <piapintoch@unal.edu.co>
2019-09-23 15:50:20 +0000

    Using a void function to create an alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main-Using a void function to print alphabet
+ *Return: 0
+ */
+
+int main(void) 
+{ 
+	print_alphabet(); 
+	getchar(); 
+	return 0; 
+} 
+void print_alphabet(void)
+{   
+	char ch;
+	for (ch = 'a'; ch < 'z'; ch++)
+		putchar(ch);
+  
+	putchar('\n');  
+}
