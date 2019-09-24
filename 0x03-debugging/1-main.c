183476ebd09d7e2b810ef0b5e3bb4809f4a2f754
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 21:27:29 +0000

    Betty error fixed

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,11 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	/**while (i < 10)
-	 *{
-	 *        putchar(i);
-	 }*/
+/*
+ *while(i < 10) This part is commented
+ *{
+ *putchar(i);
+ * }
+ */
 
 	printf("Infinite loop avoided! \\o/\n");
 
