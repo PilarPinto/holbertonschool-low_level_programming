1b9be94665aa511d578ee3d2d7a2f9dd5c79159b
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 21:59:12 +0000

    Return to the original form

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -12,18 +12,17 @@ int largest_number(int a, int b, int c)
 {
 	int largest;
 
-	if (a > b || c > b)
+	if (a >= b && b > c)
 	{
-		if (a > c)
-		{
-			largest = a;
-		}
-		else
-			largest = c;
+		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > a && a > c)
 	{
 		largest = b;
+	}
+	else
+	{
+		largest = c;
 	}
 
 	return (largest);
