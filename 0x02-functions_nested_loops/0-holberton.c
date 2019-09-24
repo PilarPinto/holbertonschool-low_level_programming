9c8b1d61505d19878f6d19f493e1bad16aba4710
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 14:46:09 +0000

    Adding all prototypes

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,19 +1,22 @@
 #include "holberton.h"
+
 /**
-*main-print the first 9 letters of Holberton with an array
-*Return: 0
-*/
+ * main - Prints letter by letter.
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
-	int i;
-	char letters[9] = "Holberton";
-	char name;
-
-	for (i = 0; i < 9; i++)
-	{
-		name = letters[i];
-		_putchar(name);
-	}
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
 	_putchar('\n');
+
 	return (0);
 }
