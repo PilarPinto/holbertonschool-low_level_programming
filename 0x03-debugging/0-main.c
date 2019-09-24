f5f34e0966faec9f0c9439622ca638776e5b7b67
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 21:32:24 +0000

    Test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * main - check the function if is positive or negative.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n = 0;
+
+	positive_or_negative(n);
+
+	return (0);
+}
