aa1abf56b7c0d02476c12f3821f4ab4f9daa9c77
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 21:36:43 +0000

    Some test

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,7 +1,7 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-void positive_or_negative(int);
+void positive_or_negative(int i);
 int largest_number(int, int, int);
 void print_remaining_days(int, int, int);
 
