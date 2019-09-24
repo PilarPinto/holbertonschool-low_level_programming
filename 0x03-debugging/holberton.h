6a2768bd7a4832c56f4c568459598d3269b3bf37
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 22:17:46 +0000

    Adding headers for eachs one

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -1,9 +1,22 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
-void positive_or_negative(int i);
-int largest_number(int a, int b, int c);
-void print_remaining_days(int a, int b, int c);
-int convert_day(int month, int day);
+#include <stdio.h>
 
-#endif /* HOLBERTON_H */
+void positive_or_negative(int i);
+#endif
+
+#ifndef LARGESTNUMBER_H
+#define LARGESTNUMBER_H
+int largest_number(int a, int b, int c);
+#endif
+
+#ifndef PRINT_REMAINING_DAYS_H
+#define PRINT_REMAINING_DAYS_H
+void print_remaining_days(int a, int b, int c);
+#endif
+
+#ifndef CONVERT_DAY_H
+#define CONVERT_DAY_H
+int convert_day(int month, int day);
+#endif
