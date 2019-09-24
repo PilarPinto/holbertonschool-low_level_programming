4bddcceb1894992c0c80a0a06a78e251f157cb93
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 23:02:37 +0000

    Using the last digit and separate the dec of the hour

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+ * jack_bauer - Prints very houd with their minutes
+ *
+ */
+void jack_bauer(void)
+{
+	int hour, minute;
+
+	for (hour = 0; hour < 24; hour++)
+	{
+		for (minute = 0; minute < 60; minute++)
+		{
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
+			_putchar(':');
+			_putchar((minute / 10) + '0');
+			_putchar((minute % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
