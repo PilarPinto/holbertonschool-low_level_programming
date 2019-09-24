b7e88f5b2f27c9930068a88454d2c9d51aef205e
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 23:41:38 +0000

    Using a math operator in a function

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+/**
+ *add - Add two numbers
+ * @s1: is the first sum value
+ * @s2: is the second sum value
+ *Return: 'add' value
+ */
+int add(int s1, int s2)
+{
+	int s;
+        s = s1 + s2;
+	
+	return (s);
+}
