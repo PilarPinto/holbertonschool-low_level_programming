b9c3f63123be590a0083ccdfefb26408bd8ffa7c
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 17:26:55 +0000

    Adding the abs function

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ *_abs- definition by parts of abs
+ * @n: is the parameter
+ *Return: 'n' value
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
