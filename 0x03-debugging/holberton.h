b6a6bdafbe8dd5ee74e2b8fb4f978e47df614aa9
PilarPinto <piapintoch@unal.edu.co>
2019-09-24 22:07:59 +0000

    Test of using param

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -2,7 +2,8 @@
 #define HOLBERTON_H
 
 void positive_or_negative(int i);
-int largest_number(int, int, int);
-void print_remaining_days(int, int, int);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int a, int b, int c);
+int convert_day(int month, int day);
 
 #endif /* HOLBERTON_H */
