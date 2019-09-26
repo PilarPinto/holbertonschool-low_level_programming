6b59ad8dbcfce4b9044077c291223d2039e5574d
PilarPinto <piapintoch@unal.edu.co>
2019-09-26 19:07:51 +0000

    Pushh the four task

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_most_numbers - Print  numbers from 0 to 9
+ * except for 2 and 4
+ */
+void print_most_numbers(void)
+{
+	int number;
+
+	for (number = '0'; number <= '9'; number++)
+	{
+		if(!(number == '2' || number == '4'))
+		{
+			_putchar(number);
+		}
+	}
+	_putchar('\n');
+}
