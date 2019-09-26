4406836806c129ee3e70a67e023f90342bf53064
PilarPinto <piapintoch@unal.edu.co>
2019-09-26 21:35:23 +0000

    Drawing a diagonal triangle

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * print_diagonal - Print a a diagonal of backslash.
+ * @n: The param of input
+ */
+ 
+void print_diagonal(int n)
+{
+	int x, y;
+	if (!(n <= 0))
+	{
+		for(x=0; x<n; x++)
+		{
+			for(y=0; y<x; y++)
+			{
+				_putchar(' ');
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}else
+		_putchar('\n');
+}
