580e4b2b953ce8cabe905346b4dcd2b4151da994
PilarPinto <piapintoch@unal.edu.co>
2019-09-26 21:39:05 +0000

    Some betty fixing

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -3,21 +3,22 @@
  * print_diagonal - Print a a diagonal of backslash.
  * @n: The param of input
  */
- 
 void print_diagonal(int n)
 {
 	int x, y;
+
 	if (!(n <= 0))
 	{
-		for(x=0; x<n; x++)
+		for (x = 0; x < n; x++)
 		{
-			for(y=0; y<x; y++)
+			for (y = 0; y < x; y++)
 			{
 				_putchar(' ');
 			}
 			_putchar(92);
 			_putchar('\n');
 		}
-	}else
+	}
+	else
 		_putchar('\n');
 }
