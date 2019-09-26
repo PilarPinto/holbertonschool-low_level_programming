2e2b315dd3344678030edfba1d83ac5d95ac58db
PilarPinto <piapintoch@unal.edu.co>
2019-09-26 18:56:20 +0000

    Using for and dec and units to print with putchar

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * more_numbers - Print dec and units for  1 to 14.
+ *
+ * No return.
+ */
+void more_numbers(void)
+{
+	int number;
+	int a = 0;
+
+	while (a < 10)
+	{
+		for (number = 0 ; number <= 14 ; number++)
+		{
+			if (number > 9)
+			{
+				putchar(number / 10 + '0');
+			}
+			putchar(number % 10 + '0');
+		}
+		putchar('\n');
+	}
+}
