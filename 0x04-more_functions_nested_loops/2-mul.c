3608525428cb8861f27c561adc66e48dbd26abe4
PilarPinto <piapintoch@unal.edu.co>
2019-09-26 14:48:46 +0000

    Funtion to multiplier two number

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * mul - Function to multiplies.
+ * @c: Is the param.
+ * Return: c the result .
+ */
+int mul(int a, int b)
+{
+	int c;
+	c = a * b;
+	return (c);
+}
