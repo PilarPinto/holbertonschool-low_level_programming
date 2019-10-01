097e85dd7488c50b6116e9242d0e8e1337ca7114
PilarPinto <piapintoch@unal.edu.co>
2019-10-01 20:56:05 +0000

    Chage the inicialitation and space

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -5,7 +5,7 @@
  * print_array - print a elements of an array.
  * @a: the array in a parameter
  * @n: Is the length of the array
- * Return: Always 0.
+ *
  */
 void print_array(int *a, int n)
 {
@@ -15,7 +15,7 @@ void print_array(int *a, int n)
 	{
 		if (i != n - 1)
 		{
-			printf("%d,  ", a[i]);
+			printf("%d, ", a[i]);
 		}
 		else
 			printf("%d", a[i]);
