fcaf92d33ef46f026785f756e0e08d96bfd2d099
PilarPinto <piapintoch@unal.edu.co>
2019-10-07 15:45:53 +0000

    Prints with the format 95 times

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * _memset - prints buffer in hexa and change values
+ * @s: the array
+ * @b: The format of element
+ * @n: Number of elements
+ * Return: changed array.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+        unsigned int i;
+
+        i = 0;
+
+	while (i < n)
+        {
+		*(s + i) = b;
+		i++;
+        }
+        return(s);
+}
