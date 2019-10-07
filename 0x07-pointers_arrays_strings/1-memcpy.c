92bf76913a6353c665d2612161be30fdc061831b
PilarPinto <piapintoch@unal.edu.co>
2019-10-07 17:20:30 +0000

    Changing some betty staff

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -11,10 +11,11 @@
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i = 0;
+
 	while (i < n)
-        {
+	{
 		*(dest + i) = *(src + i);
 		i++;
-        }
-        return(dest);
+	}
+	return (dest);
 }
