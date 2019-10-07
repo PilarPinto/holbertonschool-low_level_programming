bfae01a117997cbe4e7247f95bcc273e7a357a86
PilarPinto <piapintoch@unal.edu.co>
2019-10-07 21:42:37 +0000

    A pointer to a pointer

---
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * set_string - prints a name into another name
+ * @s: the array of Asaia
+ * @to: the array of Anne
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
