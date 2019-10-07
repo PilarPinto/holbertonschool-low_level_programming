d57a09cc3da9905478ec76ec99edc96767984f88
PilarPinto <piapintoch@unal.edu.co>
2019-10-07 17:13:43 +0000

    Test of null

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "holberton.h"
+/**
+ * _strchr - prints a cropped array
+ * @s: the array
+ * @c: The char to compare
+ * Return: Cropped array.
+ */
+char *_strchr(char *s, char c)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return('\0');
+}
