9bf400c9deb73f57a42a011662684ac12fc0d900
PilarPinto <piapintoch@unal.edu.co>
2019-10-07 16:53:07 +0000

    Change the format in a file with crecient hexa numbers

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * _memcpy - prints array into another
+ * @dest: the destinity array
+ * @src: The source array
+ * @n: Number of elements
+ * Return: changed destiny array.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i = 0;
+	while (i < n)
+        {
+		*(dest + i) = *(src + i);
+		i++;
+        }
+        return(dest);
+}
