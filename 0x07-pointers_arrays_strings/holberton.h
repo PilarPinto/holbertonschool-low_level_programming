e7bb2c704508ef91924ba5ff19c90a50e459e13e
PilarPinto <piapintoch@unal.edu.co>
2019-10-08 02:11:31 +0000

    Adding prototype

--- 0x07-pointers_arrays_strings/holberton.h
+++ 0x07-pointers_arrays_strings/holberton.h
@@ -9,6 +9,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+char *_strstr(char *haystack, char *needle);
 void set_string(char **s, char *to);
 
 #endif
