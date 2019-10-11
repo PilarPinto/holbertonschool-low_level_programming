71a371d3d07500d9979a6e0e0004d70a812e6377
PilarPinto <piapintoch@unal.edu.co>
2019-10-11 19:48:38 +0000

    Program that prints the number of arguments passed into it

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Is the main code of arg
+ * @argc: The size 0 of argv
+ * @argv: Array that pointers just the project name
+ * Return: 0 (Always success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int m;
+	(void) argv;
+        m = argc -1;
+
+	printf("%d\n", m);
+	return (0);
+}
