705cdb06a45c1523d7de8e53f4f9f8e950a15c07
PilarPinto <piapintoch@unal.edu.co>
2019-10-14 05:09:09 +0000

    Adding the counter

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -11,36 +11,43 @@ int main(int argc, char *argv[])
 {
 	
 	int cents = 0;
-	int money = atoi(argv[1])
-   
-		if(argc != 2 || money < 0){
+	int money;
+		if(argc != 2)
+		{
 			printf("Error\n");
 			return (1);
 		}
-	while (money > 0)
-	{
-		if (money - 25 >= 0)
+		money = atoi(argv[1]);
+		while (money > 0)
 		{
-			money = money -25;
-			cents++;
+			if (money - 25 >= 0)
+			{
+				money = money -25;
+				cents++;
+			}
+			else if (money - 10 >= 0)
+			{
+				money = money - 10;
+				cents++;
+			}
+			else if (money - 5 >= 0)
+			{
+				money = money - 5;
+				cents++;
+			}
+			else if (money - 2 >= 0)
+			{
+				money = money - 2;
+				cents++;
+			}
+			else if (money -1 >= 0)
+			{
+				money = money - 1;
+				cents++;
+			}
+
 		}
-		else if (money - 10 >= 0)
-		{
-			money = money - 10;
-			cents++;
-		}
-		else if (money - 5 >= 0)
-		{
-			money = money - 5;
-			cents++;
-		}
-		else if (money - 2 >= 0)
-		{
-			money = money - 2;
-			cents++;
-		}
-	}
-	printf("%d", cents);
+	printf("%d\n", cents);
    
 	return 0;
 }
