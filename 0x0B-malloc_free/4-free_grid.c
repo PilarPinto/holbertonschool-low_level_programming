095fda1e5fcb59bbec006943bce9cca3f9dec69e
PilarPinto <piapintoch@unal.edu.co>
2019-10-16 17:21:08 +0000

    Using free for liberate memoryreserved

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - Liberate reserved space
+ * @grid: the address of the two dimensional grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
