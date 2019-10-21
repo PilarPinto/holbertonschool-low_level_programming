7c483f4e807343e330739b8b026b1be1591078dc
PilarPinto <piapintoch@unal.edu.co>
2019-10-21 17:21:15 +0000

    [BETTY FIX]: Betty name description

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,5 +1,12 @@
 #include "dog.h"
-
+/**
+ * init_dog - Init the dog structure.
+ * @d: Is the structure deference variable
+ * @name: Is the name of the dog
+ * @age: Is the age of the dog
+ * @owner: Is the owner of the dog
+ * Return: Always 0.
+ */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
