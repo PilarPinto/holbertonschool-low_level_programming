a9276a06c8dda2b93a3640d74dedd18d372bb810
PilarPinto <piapintoch@unal.edu.co>
2019-10-21 19:21:41 +0000

    [FIX] Trying

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -3,40 +3,38 @@
 
 /**
  * print_dog - Show th characteristics of a dog in a list.
- * @d
- * Return: Always 0.
+ * @d: Is he deference variable
+ * Return: Nothing.
  */
 
 void print_dog(struct dog *d)
 {
-	if (!(d == NULL))
-	{
-		if ((*d).name == NULL)
-		{
-			printf("Name: %s\n", "(nil)");
-		}
-		else
-		{
-			printf("Name: %s\n", (*d).name);
-		}
-		if ((*d).age == '\0')
-		{
-			printf("Age: %s\n", "(nil)");
-		}
-		else
-		{
-			printf("Age: %f\n", (*d).age);
-		}
-		if ((*d).owner == NULL)
-		{
-			printf("Owner: nil");
-		}
-		else
-		{
-			printf("Owner: %s\n", (*d).owner);
-		}
-	}else
+	if (d == NULL)
 	{
 		return;
+	}
+	if ((*d).name == NULL)
+	{
+		printf("Name: %s\n", "(nil)");
+	}
+	else
+	{
+	printf("Name: %s\n", (*d).name);
+	}
+	if ((*d).age == '\0')
+	{
+		printf("Age: %s\n", "(nil)");
+	}
+	else
+	{
+		printf("Age: %f\n", (*d).age);
+	}
+	if ((*d).owner == NULL)
+	{
+		printf("Owner: %s\n", "(nil)");
+	}
+	else
+	{
+		printf("Owner: %s\n", (*d).owner);
 	}
 }
