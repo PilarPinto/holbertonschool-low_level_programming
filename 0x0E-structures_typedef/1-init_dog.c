cfa308694258e226d96edac81e6c4e58b7f8b869
PilarPinto <piapintoch@unal.edu.co>
2019-10-21 17:30:56 +0000

    [BETTY FIX]: Betty name description

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -5,12 +5,14 @@
  * @name: Is the name of the dog
  * @age: Is the age of the dog
  * @owner: Is the owner of the dog
- * Return: Always 0.
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (d)
+	{
 	(*d).name = name;
 	(*d).age = age;
 	(*d).owner = owner;
+	}
 }
