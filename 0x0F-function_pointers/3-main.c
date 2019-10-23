be17bb98e4960017622c3ca6fd9b17389c60d710
PilarPinto <piapintoch@unal.edu.co>
2019-10-23 20:51:16 +0000

    [Test]-The main test

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,17 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc ! = 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+	return (0);
+}
