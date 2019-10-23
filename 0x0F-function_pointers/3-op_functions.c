180acc139747260763ed0ad897218ab5e835c8ce
PilarPinto <piapintoch@unal.edu.co>
2019-10-23 20:45:40 +0000

    [Test]- The function container

---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+/**
+ * op_add - Struct op
+ *
+ * @a: The first operator
+ * @b: The second operator
+ * Return: a + b
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - Struct op
+ *
+ * @a: The first operator
+ * @b: The second operator
+ * Return: a - b
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - Struct op
+ *
+ * @a: The first operator
+ * @b: The second operator
+ * Return: a * b
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - Struct op
+ *
+ * @a: The first operator
+ * @b: The second operator
+ * Return: a / b
+ */
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+/**
+ * op_mod - Struct op
+ *
+ * @a: The first operator
+ * @b: The second operator
+ * Return: a % b
+ */
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
