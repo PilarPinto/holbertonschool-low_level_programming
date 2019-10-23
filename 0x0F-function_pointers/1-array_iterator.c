9b60dc9f7a8305c40972388618c3350465f5a0ee
PilarPinto <piapintoch@unal.edu.co>
2019-10-23 17:05:28 +0000

    [Test]- The negleted Null condition of function pointer

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -2,16 +2,17 @@
 #include "function_pointers.h"
 
 /**
- * print_elem - prints an integer
- * @elem: the integer to print
- *
+ * array_iterator - Is an array the uses a function pointer to iterate
+ * @array: Is the pointer to an array
+ * @size: Is the size of  array
+ * @action: Is the pointer function
  * Return: Nothing.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if(action != NULL)
+	if (action != NULL)
 	{
 		for (i = 0; i < size; i++)
 		{
