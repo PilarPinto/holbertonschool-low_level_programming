d7ffb8ba216ae2bc42236394d980181f996c4ef5
PilarPinto <piapintoch@unal.edu.co>
2019-10-26 00:52:38 +0000

    [Fix]-Adding condition of separator

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -17,10 +17,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
-		if (i == (n - 1))
-			printf("%d", va_arg(arguments, int));
-		else
-			printf("%d%c", va_arg(arguments, int), *separator);
+		printf("%d", va_arg(arguments, int));
+		if(i < n - 1 && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 
