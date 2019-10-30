a39f0193df9f1f9f546e47adcd26ee7e850b402d
PilarPinto <piapintoch@unal.edu.co>
2019-10-30 08:10:40 +0000

    [Fix]-Betty style fixing

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,12 +3,14 @@
  * list_len - Function that return the number of
  * elements in a linked lists
  * @h: The list pointer
+ * Return: The number of elements in the list
  */
 
 size_t list_len(const list_t *h)
 {
 	int counter = 0;
-	while(h != NULL)
+
+	while (h != NULL)
 	{
 		counter++;
 		h = h->next;
