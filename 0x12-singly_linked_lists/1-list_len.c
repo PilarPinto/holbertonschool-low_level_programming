5fff84b79a58d8e7e97cc699565262f5e90d1e68
PilarPinto <piapintoch@unal.edu.co>
2019-10-30 08:02:59 +0000

    [Feature]: A function to counter the number of elements of a linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * list_len - Function that return the number of
+ * elements in a linked lists
+ * @h: The list pointer
+ */
+
+size_t list_len(const list_t *h)
+{
+	int counter = 0;
+	while(h != NULL)
+	{
+		counter++;
+		h = h->next;
+	}
+	return (counter);
+}
