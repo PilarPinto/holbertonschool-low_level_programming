13e7cb0837ca4e4ade1d581c01acd10b4e2a888d
PilarPinto <piapintoch@unal.edu.co>
2019-10-30 07:26:45 +0000

    [Feature] - Access to the nodes

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+size_t print_list(const list_t *h)
+{
+	int n_nodes = 0;
+
+	while(h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		n_nodes++;
+	}
+
+	return (n_nodes);
+}
