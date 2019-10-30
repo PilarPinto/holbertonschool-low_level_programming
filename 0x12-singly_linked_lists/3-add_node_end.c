a7370c849a7e49176ec36259bd5d8627c376456c
PilarPinto <piapintoch@unal.edu.co>
2019-10-30 12:48:27 +0000

    [Feature]: Add to the end the node

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,43 @@
+#include "lists.h"
+/**
+ * add_node_end - Add node to the end
+ * @head: Is the pointer to the list
+ * @str: Is the pointer to element in the list
+ */
+list_t *add_node_end(list_t **head, const char *str)
+{
+	size_t counter = 0;
+	list_t *new_node = malloc(sizeof(list_t));
+	list_t *last = *head;
+
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	new_node->next = NULL;
+
+	if(*head == NULL)
+	{
+		*head = new_node;
+ 	}
+	else
+	{
+		while(last->next != 0)
+		{
+			last = last->next;
+		}
+		last->next = new_node;
+	}
+	new_node->str = strdup(str);
+
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+
+	new_node->len = counter;
+
+
+	return (new_node);
+}
