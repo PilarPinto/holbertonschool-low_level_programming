f84e98912b3acb57aeecd5456e8f1a82c99ad512
PilarPinto <piapintoch@unal.edu.co>
2019-10-30 11:02:07 +0000

    [Feature]: Adding a fuction to execute before the main

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+void myStartupFun (void) __attribute__ ((constructor));
+void myStartupFun (void)
+{
+	printf ("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
