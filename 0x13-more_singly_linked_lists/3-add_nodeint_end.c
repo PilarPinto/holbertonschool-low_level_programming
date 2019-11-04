97c69973a1afab2a178dda89c2e56b8219e55734
PilarPinto <piapintoch@unal.edu.co>
2019-11-04 21:10:01 +0000

    [Fix]: Change hte order

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -11,11 +11,11 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *last = *head;
 
-	new_node->n = n;
-	new_node->next = NULL;
-
 	if (new_node == NULL)
 		return (NULL);
+
+	new_node->n = n;
+	new_node->next = NULL;
 
 	if (*head == NULL)
 	{
