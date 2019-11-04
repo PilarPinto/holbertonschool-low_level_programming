3448e84ac4ca8e0e35446860a484e982cca8cb49
PilarPinto <piapintoch@unal.edu.co>
2019-11-04 18:54:34 +0000

    [Feature]: Free the memory used

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_listint - Liberate the space used.
+ * @head: Is the pointer to the list
+ * Return: Always 0.
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *wilc;
+
+	if (head != NULL)
+	{
+		wilc = head->next;
+		free(head);
+		head = wilc;
+	}
+}
