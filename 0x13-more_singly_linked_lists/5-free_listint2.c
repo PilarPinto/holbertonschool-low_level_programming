05289d2c325aec0a933e6f9e71d5640f6fc6192e
PilarPinto <piapintoch@unal.edu.co>
2019-11-04 18:59:13 +0000

    [Feature]: Liberate the direction of the pointer of the list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - check the code for Holberton School students.
+ * @head: is the double pointer to the linked list
+ * Return: Nothing.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *wilc;
+
+	if (head != NULL)
+	{
+		wilc = (*head)->next;
+		free(*head);
+		(*head) = wilc;
+	}
+	*head = NULL;
+}
