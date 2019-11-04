7f13005b8d1ef9979aa97640c3a863834ac687fc
PilarPinto <piapintoch@unal.edu.co>
2019-11-04 23:57:37 +0000

    [Feature]: Function to delete the first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * pop_listint - FUnction delete the first node.
+ * @head: is the direction of the pointer of the list
+ * Return: The data of the node.
+ */
+int pop_listint(listint_t **head)
+{
+	listint_t *wilc;
+	int index;
+
+	if (head == NULL)
+		return (0);
+
+
+
+	wilc = *head;
+	index = (*head)->n;
+	*head = (*head)->next;
+
+	free(wilc);
+	return (index);
+}
