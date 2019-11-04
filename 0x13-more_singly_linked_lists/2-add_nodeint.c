58fcc22e592310e70c24876863d280ce936917fb
PilarPinto <piapintoch@unal.edu.co>
2019-11-04 18:46:09 +0000

    [Feature]: Add the node to the begin

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - Add node to beginning.
+ * @head: Is the pointer to direction of the list pointer
+ * @n: Is the data in the string
+ * Return: The new node
+ */
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node = malloc(sizeof(listint_t));
+
+	new_node->n = n;
+	new_node->next = *head;
+	(*head) = new_node;
+	return (new_node);
+}
