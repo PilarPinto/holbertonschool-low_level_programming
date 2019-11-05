9acde00772e2fa65758be095133b2e6bd0181c41
PilarPinto <piapintoch@unal.edu.co>
2019-11-05 00:27:29 +0000

    [Feature]: Retunr the nth node

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,17 +1,19 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * get_nodeint_at_index - Funtion that obtain a certain index.
+ * @head: Is the linked list pointer
+ * @index: is the index that wants to found
+ * Return: That index
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int node;
+
 	if (head == NULL)
 		return (NULL);
 
-	for (node = 0; node < index; node++)
+	for (node = 0; node < index && head != NULL; node++)
 	{
 		head = head->next;
 	}
