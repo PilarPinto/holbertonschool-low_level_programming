258a1ca1ce22b8079205449e1060736a2093d885
PilarPinto <piapintoch@unal.edu.co>
2019-11-05 02:27:08 +0000

    [Fix]: Some order changing

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,9 +1,11 @@
 #include "lists.h"
 
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
+ * insert_nodeint_at_index - Check the node.
+ * @head:Is the direction of the linked list
+ * @idx: The index
+ * @n: The data
+ * Return: The new node.
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
@@ -13,22 +15,29 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (new_node == NULL)
 		return (NULL);
+
 	new_node->n = n;
 
 	if (idx == 0)
 	{
-		new_node-> next = cp;
+		new_node->next = *head;
 		*head = new_node;
+	}
+	else
+	{
+		cp = *head;
+		for (in = 0; in < idx; in++)
+		{
+			cp = cp->next;
+			if (cp == NULL || cp->next == NULL)
+			{
+				free(new_node);
+				return (NULL);
+			}
+		}
+		new_node->next = cp->next;
+		cp->next = new_node;
 		return (new_node);
 	}
-	for (in = 0; in < idx; in++)
-	{
-		if (cp == NULL || cp->next == NULL)
-			return (NULL);
-		cp = cp->next;
-	}
-	new_node->next = cp->next;
-	cp->next = new_node;
-	return (new_node);
-
+       	return (new_node);
 }
