e27f08e635bb429f2daadbbbccf0964908e8ceb9
PilarPinto <piapintoch@unal.edu.co>
2019-11-05 00:57:59 +0000

    [Feature]:The function for add

---
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * sum_listint - Sum all the data nodes
+ * @head: Is the pointer to the linked list
+ * Return: The add resut
+ */
+int sum_listint(listint_t *head)
+{
+	int add;
+
+	if (head == NULL)
+		return (0);
+	while (head != NULL)
+	{
+		add += head->n;
+		head = head->next;
+	}
+	return (add);
+}
