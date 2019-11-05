ad25fdc81acadc18d710f615c9ed4c1f1b141475
PilarPinto <piapintoch@unal.edu.co>
2019-11-05 12:43:52 +0000

    [Feature]:Funtion to set some bit

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * set_bit - Set the bit at given index.
+ * @n:The decimal number
+ * @index:The index in the binary representation
+ * Return: The bit in the position.
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int number;
+
+	if (index > 64)
+		return (-1);
+
+	number = 0;
+
+	while (number < index)
+	{
+		*n = (*n | 1 << index);
+		number++;
+	}
+	return (1);
+}
