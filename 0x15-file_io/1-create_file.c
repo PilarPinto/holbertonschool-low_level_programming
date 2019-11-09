7404ab5115b60481099f53b687d163fa6481e765
PilarPinto <piapintoch@unal.edu.co>
2019-11-09 06:43:15 +0000

    [Feature]: The function to create the file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,6 +13,9 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
+	if (text_content == NULL)
+		text_content = "";
+
 	while (text_content[ind] != 0)
 		ind++;
 
