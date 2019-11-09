e0ba107e52a769689d6a8ee135ad43ac4a2aca24
PilarPinto <piapintoch@unal.edu.co>
2019-11-09 05:04:22 +0000

    [Feature]: Functión to open and read a file

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,40 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	int fd, nr, wr;
+
+	buf = malloc(sizeof(char) * letters);
+
+	if (filename == NULL || buf == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+/*	nr = read (fd, &word, sizeof (unsigned long));*/
+	nr = read(fd, buf, letters);
+	if (nr == -1)
+	{
+		return (0);
+	}
+/*write(int fd, const void *buf, size_t count)*/
+	wr = write(STDOUT_FILENO, buf, nr);
+/*Is the POSIX*/
+	if (wr == -1)
+	{
+		return (0);
+	}
+
+	close(fd);
+	return (wr);
+}
