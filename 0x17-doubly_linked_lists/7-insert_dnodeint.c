cc30c2ad8593c0c112355db8f1f3e20192218abc
PilarPinto <piapintoch@unal.edu.co>
2019-12-15 15:15:18 +0000

    [Feature]-Insert node at nth position

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,38 @@
+#include "lists.h"
+/**
+ * insert_dnodeint_at_index - Insert a node in the postion
+ * @h: Pointer to direction of the head
+ * @n: The data integer
+ * @idx: Position at the insert the new node
+ * Return: The direction of the new node
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	dlistint_t *new_mem, *res_mem = *h;
+	unsigned int counter = 0;
+
+	if (idx == 0)
+		return (add_dnodeint(h, n));
+
+	while (counter < idx)
+	{
+		res_mem = res_mem->next;
+		if (res_mem == NULL)
+			return (NULL);
+	}
+	if (res_mem->next == NULL)
+		return (add_dnodeint(h, n));
+
+	new_mem = malloc(sizeof(dlistint_t));
+
+	if (new_mem == NULL)
+		return (NULL);
+
+	new_mem->n = n;
+	new_mem->next = res_mem->next;
+	new_mem->prev = res_mem;
+	res_mem->next->prev = new_mem;
+	res_mem->next = new_mem;
+
+	return (new_mem);
+}
