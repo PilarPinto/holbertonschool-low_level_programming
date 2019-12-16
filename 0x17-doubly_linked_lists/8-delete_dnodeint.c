adc153b9c1d8fccc8c19e7c429be7a3b513f953a
PilarPinto <piapintoch@unal.edu.co>
2019-12-16 04:59:26 +0000

    try

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,38 +7,31 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *res_mem = *head, *p1, *p2;
-	unsigned int counter;
+	dlistint_t *res_mem = *head;
+	unsigned int counter = 0;
 
 	if (head == NULL || *head == NULL)
 		return (-1);
 
 	if (index == 0)
 	{
-		(*head) = res_mem->next;
-		(*head)->prev = NULL;
+		*head = res_mem->next;
+		if (res_mem->next == NULL)
+			return (-1);
+		res_mem->next->prev = NULL;
 		free(res_mem);
 		return (1);
 	}
 
 	while (counter < index)
 	{
-		if (res_mem == NULL)
+		if (res_mem->next == NULL)
 			return (-1);
 		res_mem = res_mem->next;
 		counter++;
 	}
-	if (res_mem->next == NULL)
-	{
-		p1 = res_mem->prev;
-		p1->next = NULL;
-		free(res_mem);
-		return (1);
-	}
-	p1 = res_mem->next;
-	p2 = res_mem->prev;
+	if (res_mem->next)
+		res_mem->next->prev = res_mem->prev;
 	free(res_mem);
-	p2->next = p1;
-	p1->prev = p2;
 	return (1);
 }
