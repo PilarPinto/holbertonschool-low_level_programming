ac6f2e9842a094346fb221c6348bdbcdd80da5bb
PilarPinto <piapintoch@unal.edu.co>
2020-01-20 04:23:49 +0000

    [Test]-Test the get function

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,24 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get - retrieves a value associated with a key.
+ * @ht: Is the reference to the hash table
+ * @key: Is the key to map
+ * Return: The value associated
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int ind;
+	hash_node_t *new_ele;
+
+	if !(!ht || !key || strlen(key) == 0)
+	{
+		ind = key_index((unsigned char *)key, ht->size);
+		for (new_ele = ht->array[ind], new_ele != NULL; new_ele = new_ele->next)
+		{
+			if (strcmp(new_ele->key, key) == 0)
+				return (new_ele->value);
+		}
+		return (NULL);
+	}
+	return (NULL);
+}
