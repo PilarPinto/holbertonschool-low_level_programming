a2561c30a6caff023950ee0ca75f6c87c47cb1e5
PilarPinto <piapintoch@unal.edu.co>
2020-02-21 23:05:27 +0000

    [Feature]- Is the Islend perimeter counter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,17 +1,13 @@
 #!/usr/bin/python3
-"""
-Perimeter of the Island
-"""
-
-
 def island_perimeter(grid):
-        count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[i])):
-                if grid[i][j] is 1:
-                    count += 4
-                    if j > 0 and grid[i][j - 1]:
-                        count -= 2
-                    if i > 0 and grid[i - 1][j]:
-                        count -= 2
-            return count
+    '''Is the function to find the island perimeter'''
+    count = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j] is 1:
+                count += 4
+                if j > 0 and grid[i][j - 1]:
+                    count -= 2
+                if i > 0 and grid[i - 1][j]:
+                    count -= 2
+    return count
